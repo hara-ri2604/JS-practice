83ba8440624e2d54a7a0989bd4e66e7c6b9e65a1
Hara Riku <hara-ri2604@ibjapan.jp>
2026-04-24 15:22:26 +0900

    02 変数/定数と演算

---
+++ 02/02.js
@@ -0,0 +1,7 @@
+//const weight = 72;
+const height = 1.76;
+//height = 188;
+//console.log('体重' + weight + 'kg/身長' + height * 100 + 'cmのBMI');
+
+let weight = 72;
+weight = 75
---
+++ 02/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// console.log('Hello' + ' ' + 'world' + '!!');
+// console.log('1たす1は' + 2);
+
+// // BMI：体重[kg]/(身長[m]*身長[m])
+// console.log('体重' + 72 + 'kg／身長' + 176 + 'cmのBMI：');
+// console.log(72 / (1.76 * 1.76)); 
+
+// let weight = 50;
+// weight = 55;
+// console.log(weight);
+let x = 10;
+x *= 5;
+console.log(x);
+
+x++;
+console.log(x);
