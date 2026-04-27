07c90b330ad06cc832b4039731a4f64f6a6d14bd
Hara Riku <hara-ri2604@ibjapan.jp>
2026-04-27 16:08:14 +0900

    08終了　

---
+++ 08/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// console.log('Hello World!!');
+const p1 = document.getElementById('dom');
+console.log (p1.textContent);  // → Document Object Modelの略です。
+
+const a = document.getElementById('link'); // a要素
+console.log(a);
+
+const p = a.parentElement;  // 親要素
+console.log(p);
+
+const img = a.firstElementChild;  // 最初の子要素
+console.log(img);
